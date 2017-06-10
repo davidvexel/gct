11c890c0fcb2a575f3b4791e8c1f20e013e2bb01
David Leal <dleal@junkyard.mx>
2017-06-10 09:13:03 -0500

    Creating orders tours class and ajax helper

---
+++ include/ajax-helper.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Ajax helper
+ * 
+ * Get tours price by zone
+ *
+ * @author David Leal
+ */
+
+include "Connections/conexion.php";
---
+++ include/class-orders.php
@@ -0,0 +1,23 @@
+<?php
+/**
+* Manage tour orders
+*
+* @author David Leal
+* @package 1.0
+*/
+include "Connections/conexion.php";
+
+class Orders_Tours {
+	
+	/**
+	 * Save order data
+	 * @param array $data
+	 */
+	public static function save_order( $data ) {
+		var_dump( $data );
+	}
+
+	// send mails
+
+	// process payment
+}
